7c10ca20bf9d98e8700b4a3aca03f9d1dc1c2326
Haden Watne <haden@hadenwatne.com>
2023-02-06 12:16:15 -0700

    Improve colors, tabbing

--- Frontend/src/app/components/ui/input.directive.ts
+++ Frontend/src/app/components/ui/input.directive.ts
@@ -7,6 +7,6 @@ export class InputDirective {
   constructor(private element: ElementRef) {}
 
   @HostBinding() get class() {
-    return 'py-2 px-3 rounded-md border border-gray-200 focus:outline-none focus:ring focus:ring-indigo-400';
+    return 'py-2 px-3 rounded-md border border-gray-200 focus:outline-none focus:ring focus:ring-[#39A0ED]';
   }
 }
